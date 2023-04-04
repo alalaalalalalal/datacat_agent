915cff7e4daea727b76080c9efa787baf5a3da95
alalaalalalalal <125862867+alalaalalalalal@users.noreply.github.com>
2023-04-04 15:11:08 +0900

    실행 방식 업데이트

--- src/main/java/com/main/datacat_agent/DatacatAgentApplication.java
+++ src/main/java/com/main/datacat_agent/DatacatAgentApplication.java
@@ -30,15 +30,6 @@ public class DatacatAgentApplication implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
-        // SSH2AWS ssh2aws = new SSH2AWS();
-		// String[] commands = {"sudo ssh -t -t -i SIT_DEV_DP_KEY_Bastion_Virginia.pem ubuntu@10.157.16.40","k top node --no-headers=true | awk '{print $3}' | sed 's/[^0-9]//g' | awk '{if ($1 >= 0) print $0}' | wc -l"};
-		// System.out.println(ssh2aws.getSSHResponse(commands));
-		// // ssh2aws.getSSHResponse("d_bastion_vir", "k top node --no-headers=true | awk '{print $3}' | sed 's/[^0-9]//g' | awk '{if ($1 >= 0) print $0}' | wc -l");
-		// System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@시작했다@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-		
-		// StringBuilder result = getDatacatAgentService().execShellScript("kubectl top nodes");
-		// System.out.println(result.toString());
-
 		while(true){
 			List<ScriptEntity> scriptList = getDatacatAgentService().readScript();
 			for(ScriptEntity scriptEntity : scriptList){
@@ -57,7 +48,6 @@ public class DatacatAgentApplication implements CommandLineRunner {
 					if(lastExcutionAt == null ){
 						scriptResult = getDatacatAgentService().execShellScript(scriptCommand);
 						result = scriptResult.toString();
-						System.out.println(result);
 						if(!result.equals("0")){//정상
 							getDatacatAgentService().insertScriptResult( new ExecutionLogEntity(1, result, timestamp, scriptId));
 						}else{//비정상
@@ -67,7 +57,7 @@ public class DatacatAgentApplication implements CommandLineRunner {
 					}else{
 						String lastExecStamp = String.valueOf(lastExcutionAt.getTime());
 						Date lastExecDate = new Date(Long.parseLong(lastExecStamp));
-						if(lastExecDate.compareTo(date)>=0){ //스크립트 마지막 실행시간 + 인터벌이 현재시간보다 이후일 경우에만 실행
+						// if(lastExecDate.compareTo(date)>=0){ //스크립트 마지막 실행시간 + 인터벌이 현재시간보다 이후일 경우에만 실행
 
 							//스크립트 실행
 							scriptResult = getDatacatAgentService().execShellScript(scriptCommand);
@@ -80,7 +70,7 @@ public class DatacatAgentApplication implements CommandLineRunner {
 							}
 				
 							
-						}
+						// }
 					}
 					
 				}	
