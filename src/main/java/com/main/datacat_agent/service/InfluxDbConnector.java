33a3c6e4d9285539a4e85e248b35a397a53c2650
alalaalalalalal <125862867+alalaalalalalal@users.noreply.github.com>
2023-04-17 12:49:40 +0900

    influx 시간 제한 변경
    https://stackoverflow.com/questions/50922473/influxdbioexception-java-net-sockettimeoutexception-timeout

--- src/main/java/com/main/datacat_agent/service/InfluxDbConnector.java
+++ src/main/java/com/main/datacat_agent/service/InfluxDbConnector.java
@@ -1,7 +1,11 @@
 package com.main.datacat_agent.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
+
+import okhttp3.OkHttpClient;
 
 public class InfluxDbConnector {  //연결용 클래스 입니다.
 	
@@ -12,6 +16,12 @@ public class InfluxDbConnector {  //연결용 클래스 입니다.
     private static InfluxDbConnector inf;
     private InfluxDB db = null;
 	
+    OkHttpClient.Builder client = new OkHttpClient.Builder()
+    .connectTimeout(1, TimeUnit.MINUTES)
+    .readTimeout(1, TimeUnit.MINUTES)
+    .writeTimeout(1, TimeUnit.MINUTES)
+    .retryOnConnectionFailure(true);
+    
     private InfluxDbConnector() {	 }
 	
     public static InfluxDbConnector getInstance() {
@@ -25,14 +35,14 @@ public class InfluxDbConnector {  //연결용 클래스 입니다.
     }
 	
     private void tryToConnecting() {
-        db = InfluxDBFactory.connect(DB_URL, DB_ID, DB_PWD);
+        db = InfluxDBFactory.connect(DB_URL, DB_ID, DB_PWD, client);
     }
 	
     public InfluxDB getDb() {
         try {
             if(db == null || db.ping() == null){
                 synchronized(inf) {
-                    db = InfluxDBFactory.connect(DB_URL, DB_ID, DB_PWD);
+                    db = InfluxDBFactory.connect(DB_URL, DB_ID, DB_PWD, client);
                 }
             }			
         } catch (Exception e) {
