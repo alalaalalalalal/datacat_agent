5b4428b16b4889901af6dc42afd1a8f59759ccfa
alalaalalalalal <125862867+alalaalalalalal@users.noreply.github.com>
2023-04-18 13:36:31 +0900

    마지막 커밋

--- src/main/java/com/main/datacat_agent/service/InfluxDbConnector.java
+++ src/main/java/com/main/datacat_agent/service/InfluxDbConnector.java
@@ -1,14 +1,18 @@
 package com.main.datacat_agent.service;
 
+import java.util.List;
+
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
+import org.influxdb.dto.QueryResult.Result;
+import org.influxdb.dto.QueryResult.Series;
 
 public class InfluxDbConnector {  //연결용 클래스 입니다.
 	
-    private final String DB_URL = "http://10.157.16.71:8086";
-    // private final String DB_URL = "http://DEV-Influx-Connect-IMSI-ba7f50050f9bbe38.elb.us-east-1.amazonaws.com:8086";
+    private final String DB_URL = "http://10.157.16.71:8086"; //서버용
+    // private final String DB_URL = "http://DEV-Influx-Connect-IMSI-ba7f50050f9bbe38.elb.us-east-1.amazonaws.com:8086"; //로컬용
     private final String DB_ID = "admin";
     private final String DB_PWD = "admin";
 
@@ -17,7 +21,15 @@ public class InfluxDbConnector {  //연결용 클래스 입니다.
         
         influxDB.setDatabase("isl");
         QueryResult queryResult = influxDB.query(new Query(q));
-        System.out.println(queryResult);
+        List<Result> resultList = queryResult.getResults();
+        for(Result result : resultList ){
+            for(Series value : result.getSeries()){
+                for(List object : value.getValues()){
+                    System.out.println(object.get(2));
+                }
+            }
+        }
+       
         influxDB.close();
     }
 }
