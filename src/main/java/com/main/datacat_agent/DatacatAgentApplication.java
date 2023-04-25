f71030a87ef8b2865a0beaae5d9033e9377d7a2b
alalaalalalalal <125862867+alalaalalalalal@users.noreply.github.com>
2023-04-25 14:44:08 +0900

    test

--- src/main/java/com/main/datacat_agent/DatacatAgentApplication.java
+++ src/main/java/com/main/datacat_agent/DatacatAgentApplication.java
@@ -86,7 +86,7 @@ public class DatacatAgentApplication implements CommandLineRunner {
 		String[] scriptCommand = {"/bin/sh", "-c", scriptEntity.getCommand()};
 		log.info("스크립트 log={}", scriptCommand[2]);
 		int scriptId = Long.valueOf(scriptEntity.getPid()).intValue();
-		log.info("스크립트 id={}",scriptId);
+		
 		Timestamp lastExcutionAt = getDatacatAgentService().readScriptExecutionAt(scriptId);
 		StringBuilder scriptResult = new StringBuilder();
 		String result = "";
@@ -115,7 +115,7 @@ public class DatacatAgentApplication implements CommandLineRunner {
 			result_tmp.setTime(cal.getTime().getTime());
 
 			if(cal.getTime().compareTo(result_tmp)>0){ //만약 최종시작일 + 인터벌이 현재 시각보다 클경우 (마지막 실행 2시  인터벌 120분 현재시각 4시 30분이면  2시+120분 = 4시 이므로 실행 해야함)
-
+				log.info("스크립트 id={}",scriptId);
 				if(!result.equals("0")){//정상
 					getDatacatAgentService().insertScriptResult( new ExecutionLogEntity(1, result, timestamp, scriptId));
 				}else{//비정상
