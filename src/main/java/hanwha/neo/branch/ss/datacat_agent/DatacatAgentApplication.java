c1f0316c80741524919370085eba05f61b269c80
alalaalalalalal <125862867+alalaalalalalal@users.noreply.github.com>
2023-07-24 10:59:45 +0900

    수신자별 메일 전송

--- src/main/java/hanwha/neo/branch/ss/datacat_agent/DatacatAgentApplication.java
+++ src/main/java/hanwha/neo/branch/ss/datacat_agent/DatacatAgentApplication.java
@@ -108,13 +108,13 @@ public class DatacatAgentApplication implements CommandLineRunner {
 			MailSender mailSender = new MailSender();
 			for (String receiver : recvList){
 				log.info("리시버 {}" ,receiver);
-				// receivers[0] = new WsRecipient();
-				// receivers[0].setSeqID(1);
-				// receivers[0].setRecvType("TO");
-				// receivers[0].setRecvEmail(reciver);
-				// String content = messageMailEntity.getMailContents();
-				// mailSender.sendTextMail(MailEndpoint, messageMailEntity.getMailSubject(), sender, receivers,
-				// 	content);
+				receivers[0] = new WsRecipient();
+				receivers[0].setSeqID(1);
+				receivers[0].setRecvType("TO");
+				receivers[0].setRecvEmail(receiver);
+				String content = messageMailEntity.getMailContents();
+				mailSender.sendTextMail(MailEndpoint, messageMailEntity.getMailSubject(), sender, receivers,
+					content);
 			}
 
 			//수신자 지정
